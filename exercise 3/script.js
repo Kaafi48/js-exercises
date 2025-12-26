338fb82aa89e7c3af22afc699381bc88e574c914
Hanad Yusuf <abdikafihonest123@gmail/com>
2025-12-26 21:09:01 +0300

    exercise3

---
+++ exercise 3/script.js
@@ -0,0 +1,11 @@
+
+
+
+// logic 
+
+let isSunyy = true;
+let hasWeekend = true;
+console.log(isSunyy && hasWeekend);
+console.log(isSunyy || hasWeekend);
+console.log(!isSunyy);
+console.log(!hasWeekend);
