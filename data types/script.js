0b724454d7ebb5e43ca88f7f19f61638d2ec81e7
Hanad Yusuf <abdikafihonest123@gmail/com>
2025-12-26 19:09:29 +0300

    js exercises

---
+++ data types/script.js
@@ -0,0 +1,16 @@
+
+
+// string  - qoraal 
+
+let nickname = "faarax";
+console.log(nickname);
+
+// number - tiro
+
+const age = 29;
+console.log(age);
+
+//  boolean - true and false  sax iyo qalad
+
+let isStudent = true;
+console.log(isStudent);
---
+++ variable/index.js
@@ -0,0 +1,14 @@
+
+let age = 30;
+// age = 50;  waad badli karta values ka 
+console.log(`your age is ${age} years`);
+
+const name = "Abdikafi";
+// name = "muuse"; lama badali karo values ka 
+console.log(`magacaga waa ${name}`);
+
+var tariq = 2005;
+// tariq = 2001; waa la badli kara xogta iyadana 
+console.log("waxaad dhalata " + tariq);
+
+
